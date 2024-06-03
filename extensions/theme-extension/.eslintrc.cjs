e941c42143b4ab4c952e3e0c67954ce8ae614e1b
Kenichi Yukihira <k.yukihira@diamondhead.tech>
2024-06-03 18:22:37 +0900

    ESLintが動いた

---
+++ extensions/theme-extension/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended"],
+  rules: {
+    // ここに必要なルールを追加します
+  },
+};
