4037cf0cba4fb5d28cde14cf99a8ec324a3a0f45
Kenichi Yukihira <k.yukihira@diamondhead.tech>
2024-06-03 12:46:54 +0900

    prettierを導入

---
+++ extensions/theme-extension/.prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  useTabs: false,
+  tabWidth: 2,
+  endOfLine: "lf",
+  printWidth: 90,
+  singleQuote: false,
+  trailingComma: "all",
+  plugins: ["prettier-plugin-organize-imports"],
+};
--- extensions/theme-extension/vite.config.ts
+++ extensions/theme-extension/vite.config.ts
@@ -1,19 +1,14 @@
-import { defineConfig } from "vite";
-import shopify from "vite-plugin-shopify";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import pageReload from "vite-plugin-page-reload";
+import shopify from "vite-plugin-shopify";
 
 export default defineConfig({
   plugins: [
     shopify(),
     react(),
     pageReload(
-      [
-        "/tmp/extension.update",
-        "./blocks/**/*",
-        "./locales/**/*",
-        "./snippets/**/*",
-      ],
+      ["/tmp/extension.update", "./blocks/**/*", "./locales/**/*", "./snippets/**/*"],
       {
         // 短すぎると適切に反映されないので、十分な時間を取る
         delay: 1800,
